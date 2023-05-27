fdee9dc0b4d54d05ad47b1453e47f37d32abf4ed
shashankmadaan379 <codebreaker379@gmail.com>
2023-05-27 12:03:25 +0530

    strategy design pattern

--- strategyDesignPattern.js
+++ strategyDesignPattern.js
@@ -2,27 +2,35 @@
 // strategy (car,bus,train,swim)
 // Person => car,bus,train,swim => office
 
-function JobPerson(strategy) {
+function Shipping(strategy) {
   this.strategy = strategy;
 }
-JobPerson.prototype.goToOffice = function () {
+Shipping.prototype.getShippingProductCost = function () {
   return this.strategy();
 };
+
+//group of strategies
 const trainStrategy = function () {
-  console.log("Train");
+  console.log("It cost 200$ via train.");
 };
 const busStrategy = function () {
-  console.log("Bus");
+  console.log("It cost 100$ via bus.");
 };
 const carStrategy = function () {
-  console.log("Car");
+  console.log("It cost 150$ via car.");
 };
 const swimStrategy = function () {
-  console.log("Swim");
+  console.log("cost 20$");
 };
 
-const officeViaTrain = new JobPerson(trainStrategy);
-const officeViaCar = new JobPerson(carStrategy);
-const officeViaBus = new JobPerson(busStrategy);
-console.log(officeViaCar.goToOffice());
-console.log(officeViaTrain.goToOffice());
+const arr = [];
+const shippedViaTrain = new Shipping(trainStrategy);
+arr.push(shippedViaTrain);
+const shippedViaCar = new Shipping(carStrategy);
+arr.push(shippedViaCar);
+const shippedViaBus = new Shipping(busStrategy);
+arr.push(shippedViaBus);
+
+arr.forEach(function (shipping) {
+  console.log(shipping.getShippingProductCost());
+});
